97ce17bd14090d1377dd558364966e6955f6cac8
Viktoria Asvetim <asvetim@mail.ru>
2021-10-11 15:48:37 +0300

    <первый 11 окт>

---
+++ js/script.js
@@ -0,0 +1,3 @@
+jQuery(function($){$("form").submit(function(){var formID=$(this).attr('id');var formNm=$('#'+formID);$.ajax({type:"POST",url:'mail.php',data:formNm.serialize(),success:function(data){$(formNm).html(data);},error:function(jqXHR,text,error){$(formNm).html(error);}});return false;});});
+
+
---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "asvetim@mail.ru"; 
+    $sendfrom   = "info@mail.ru"; 
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "<b> $formData </b><br> <b>Имя отправителя:</b> $name <br><b>Телефон:</b> $phone";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<center><p class="success"><span><b>Спасибо!</b><br>Скоро с вами свяжется консультант для уточнения деталей.</span></p></center>';
+    }
+    else 
+    {
+    echo '<center><p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p></center>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
